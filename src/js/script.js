aa3d38cfe743797b35acad7d6a21b40679566a28
Don Harrison <donald.harrison@utah.edu>
2022-11-07 12:21:06 -0800

    fixed csv parsing, checking data #15

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,16 @@
 //attributes
 let globalData = {};
 
-Promise.all([d3.csv("./data/movies(1986-2016).csv"), d3.csv("./data/Highest Holywood Grossing Movies.csv")]).then(data => {
+Promise.all([d3.csv("../../data/movies(1986-2016).csv"), d3.csv("../../data/Highest Holywood Grossing Movies.csv")]).then(data => {
     //Load in movies csv
     globalData.movies = data[0];
     globalData.grossing = data[1];
+
+    console.log(globalData);
+
+    let grossingMovieNames = Array.from(d3.group(globalData.grossing, d => d["Title"]).keys());
+
+    console.log(globalData.movies.filter(movie => grossingMovieNames.find(d => d == movie.name)));
+    console.log(grossingMovieNames);
 });
+
