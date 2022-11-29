bcfcc8f00fa635c96104b95ad32fe80c32d629a0
Don Harrison <donald.harrison@utah.edu>
2022-11-29 10:01:06 -0800

    #38 stream chart rendering, badly

--- src/js/streamchart.js
+++ src/js/streamchart.js
@@ -46,28 +46,28 @@ class StreamChart{
         this.dateGenreRevMap = this.getTotalRevenueFromGenreByYear();
     
         const keys = this.allMoviesGenres;
+        let parseTime = d3.timeParse("%B %d, %Y");
 
         // Add X axis
         const x = d3.scaleTime()
         .domain(d3.extent(this.globalFlags.grossing, function(d) { 
-            return d["Release Date"];
+            return new Date(parseTime(d["Release Date"]));
         }))
         .range([ 0, width ]);
         svg.append("g")
         .attr("transform", `translate(0, ${height})`)
         .call(d3.axisBottom(x).ticks(5));
 
-        let parseTime = d3.timeParse("%B %d, %Y");
         let timeFormat = d3.timeFormat("%B %Y");
         let parseTime2 = d3.timeParse("%B %Y");
 
-        console.log(x(parseTime("Fri Jun 01 2018 00:00:00 GMT-0700 (Pacific Daylight Time)")));
-
+        let extent = d3.extent(this.globalFlags.grossing, function(d) { 
+            return parseInt(d["World Sales (in $)"]);
+        });
+        console.log(extent);
         // Add Y axis
         const y = d3.scaleLinear()
-        .domain(d3.extent(this.globalFlags.grossing, function(d) { 
-            return parseInt(d["World Sales (in $)"]);
-        }))
+        .domain(extent)
         .range([ height, 0 ]);
 
         svg.append("g")
@@ -76,13 +76,22 @@ class StreamChart{
         // color palette
         const color = d3.scaleOrdinal()
         .domain(keys)
-        .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf'])
+        .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf']);
+
+        let sortedData = Array.from(this.dateGenreRevMap.values()).sort((a,b) => {
+            let date1 = new Date(a["date"]);
+            let date2 = new Date(b["date"]);
+
+            return date1 - date2;
+        });
+
+        console.log(sortedData);
 
         //stack the data?
         const stackedData = d3.stack()
         .offset(d3.stackOffsetSilhouette)
-        .keys(keys)
-        (this.dateGenreRevMap.values());
+        .keys(keys).value((obj, key) => obj[key])
+        (sortedData);
 
         console.log(stackedData);
 
@@ -93,6 +102,7 @@ class StreamChart{
         .join("path")
         .attr("transform",
         `translate(100, -400)`)
+        .style("stroke", "none")
         .style("fill", function(d) { 
             return color(d.key); })
             .attr("d", d3.area()
@@ -108,72 +118,6 @@ class StreamChart{
             })
         )
     }
-
-    drawMockChart(){
-        // set the dimensions and margins of the graph
-         const margin = {top: 20, right: 30, bottom: 30, left: 60},
-         width = 800 - margin.left - margin.right,
-         height = 500 - margin.top - margin.bottom;
- 
-         // append the svg object to the body of the page
-         const svg = d3.select(".streamChart")
-         .append("svg")
-         .attr("width", width + margin.left + margin.right)
-         .attr("height", height + margin.top + margin.bottom)
-         .append("g")
-         .attr("transform",
-             `translate(${margin.left}, ${margin.top})`);
- 
-         // Parse the Data
-         d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered_wide.csv").then( function(data) {
- 
-         // List of groups = header of the csv files
-         const keys = data.columns.slice(1)
-
-         console.log(keys);
- 
-         // Add X axis
-         const x = d3.scaleLinear()
-         .domain(d3.extent(data, function(d) { return d.year; }))
-         .range([ 0, width ]);
-         svg.append("g")
-         .attr("transform", `translate(0, ${height})`)
-         .call(d3.axisBottom(x).ticks(5));
- 
-         // Add Y axis
-         const y = d3.scaleLinear()
-         .domain([-100000, 100000])
-         .range([ height, 0 ]);
-         svg.append("g")
-         .call(d3.axisLeft(y));
- 
-         // color palette
-         const color = d3.scaleOrdinal()
-         .domain(keys)
-         .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf'])
- 
-         //stack the data?
-         const stackedData = d3.stack()
-         .offset(d3.stackOffsetSilhouette)
-         .keys(keys)
-         (data)
-
-         console.log(stackedData);
-         // Show the areas
-         svg
-         .selectAll("mylayers")
-         .data(stackedData)
-         .join("path")
-         .attr("transform",
-         `translate(100, -500)`)
-         .style("fill", function(d) { return color(d.key); })
-         .attr("d", d3.area()
-             .x(function(d) { return x(d.data.year); })
-             .y0(function(d) { return y(d[0]); })
-             .y1(function(d) { return y(d[1]); })
-         )
-         })
-     }
 
     //Return Array containing all Genres that appear in grossing
     getAllGrossingGenres(){
